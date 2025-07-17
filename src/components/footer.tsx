4097c1370046bed4b7f44d5479688a1630eceab3
princessSarmica <93124427+princessSarmica@users.noreply.github.com>
2025-07-17 18:36:53 +0200

    Added Prisma ORM and DB connection

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="flex justify-center items-center mt-auto border-black/10 border-t h-[40px] px-5">
+            Footer
+        </div>
+    );
+}
