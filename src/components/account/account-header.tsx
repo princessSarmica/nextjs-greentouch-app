b8b51ada8b857768c4ad06082900ed9286729063
princessSarmica <93124427+princessSarmica@users.noreply.github.com>
2025-08-28 21:54:54 +0200

    Updated account header to display user data

--- src/components/account/account-header.tsx
+++ src/components/account/account-header.tsx
@@ -1,12 +1,31 @@
-// components/account/account-header.tsx
-import { cn } from "@/lib/utils";
+import { auth } from "@/lib/auth"; // path to your Better Auth server instance
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function AccountNavbar(){
+export default async function AccountHeader(){
+
+  const session = await auth.api.getSession({
+      headers: await headers()
+  })
+
+  if(!session) {
+      redirect("/sign-in")
+  }
+
+  const name = session?.user?.name ?? "No name found";
+  const position = (session?.user?.position ?? "No position found");
 
   return (
-    <section className={cn("w-full border-b")}>
-      <div className="h-40 w-full bg-[#65a164]/60" />
-    </section>
+    <div className="w-full h-40 bg-primary/60">
+      <div className="mx-auto w-full max-w-5xl h-full px-4 flex items-end">
+        <div className="py-15">
+          <h1 className="text-2xl font-semibold text-foreground">
+            {name}
+          </h1>
+          <p className="text-base font-semibold text-foreground">{position}</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
