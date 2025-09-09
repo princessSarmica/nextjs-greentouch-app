a59dbf3d18b5958730a1342e064a456a250df308
princessSarmica <93124427+princessSarmica@users.noreply.github.com>
2025-09-09 09:33:10 +0200

    Added dialog component for revoking sessions

--- src/components/account/security/revoke-sessions-button.tsx
+++ src/components/account/security/revoke-sessions-button.tsx
@@ -4,6 +4,16 @@ import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 export default function RevokeOtherSessions(){
 
@@ -12,25 +22,44 @@ export default function RevokeOtherSessions(){
 
   return (
     <div>
-      <Button
-        onClick={async () => {
-        const res = await fetch("/api/security/revoke-other-sessions", { method: "POST" });
-        if (res.ok) toast.success("Other sessions revoked");
-        else {
-          toast.error("Failed to revoke other sessions");
-          authClient.signOut({
-            fetchOptions: {
-              onError: () => {
-                session.refetch();
-                router.push("/sign-in"); // redirect to sign-in page
-              },
-            },
-          })
-        }
-      }}
-        >
-        Revoke Other Active Sessions
-      </Button>
+      <Dialog>
+        <DialogTrigger>
+          <Button>Revoke Other Active Sessions</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              Revoking all other active sessions will require you to sign in again on those devices.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button
+                onClick={async () => {
+                const res = await fetch("/api/security/revoke-other-sessions", { method: "POST" });
+                if (res.ok) {
+                  toast.success("Other sessions revoked");
+                } else {
+                  toast.error("Failed to revoke other sessions");
+                  authClient.signOut({
+                    fetchOptions: {
+                      onError: () => {
+                        session.refetch();
+                        router.push("/sign-in"); // redirect to sign-in page
+                      },
+                    },
+                  })
+                }
+              }}
+                >
+                Continue
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
