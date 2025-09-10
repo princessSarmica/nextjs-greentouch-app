1f231940c4fc369fad3aaf3e324d10aa09eab481
princessSarmica <93124427+princessSarmica@users.noreply.github.com>
2025-09-10 11:09:19 +0200

    Fixed repeated getSession calls per request

--- src/lib/get-session.ts
+++ src/lib/get-session.ts
@@ -1,8 +1,14 @@
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
+import { cache} from "react";
 
-export async function getServerSession() {
+// A cached function to get the server session
+// This prevents multiple calls to getSession during a single request
+export const getServerSession = cache(async () => {
+
+    //console.log("getServerSession called");
+
     return await auth.api.getSession({
         headers: await headers()
     })
-}
+})
