ef7cac83a85ffd40a639a73d3976bc4b0da1507b
princessSarmica <93124427+princessSarmica@users.noreply.github.com>
2025-10-26 09:20:11 +0100

    Added loading screen to account pages

---
+++ src/app/account/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center pt-20 bg-[#f5f5f5]">
+      <div className="animate-spin rounded-full h-16 w-16 border-4 border-t-[#1F566E] border-[#d3d3d3]"></div>
+    </div>
+  );
+}
